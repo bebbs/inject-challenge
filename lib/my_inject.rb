f413266dcf03f72746c6bbea74061027983be9b6
Josh Bebbington <josh@focalwebsites.com>
2015-01-03 21:59:19 +0000

    Started building out iterative my_itr_inject method, based on recursive method

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,14 +1,23 @@
 class Array
 
+  # Recursive inject method
   def my_rec_inject(*args, &block)
     arr = self.dup
     temp, next_value = arr.shift, 0 if args[0].is_a?(Symbol) || args.empty?
     temp, next_value = args[0], 0 if !args[0].is_a?(Symbol) && !args[0].nil?
-    args.map{ |arg| block = arg.to_proc if arg.is_a?(Symbol) }
+    args.map{|arg| block = arg.to_proc if arg.is_a?(Symbol)}
     return temp if arr.empty?
 
-    temp = block.call temp, arr[next_value]
+    block_result = block.call temp, arr[next_value]
 
-    arr.drop(1).my_rec_inject(temp, args, &block)
+    arr.drop(1).my_rec_inject(block_result, args, &block)
+  end
+
+  # Iterative inject method
+  def my_itr_inject(*args, &block)
+    arr = self.dup
+    temp = self[0] if args[0].is_a?(Symbol) || args.empty?
+    temp = args[0] if !args[0].is_a?(Symbol) && !args[0].nil?
+    args.map{|arg| block = arg.to_proc if arg.is_a?(Symbol)}
   end
 end
--- spec/my_inject_spec.rb
+++ spec/my_inject_spec.rb
@@ -6,16 +6,20 @@ describe Array do
     it 'can add numbers' do
       expect([1,2,3,4].inject {|memo, item| memo + item}).to eq 10
       expect([1,2,3,4].my_rec_inject {|memo, item| memo + item}).to eq 10
+      expect([1,2,3,4].my_itr_inject {|memo, item| memo + item}).to eq 10
+
     end
 
     it 'can minus numbers' do
       expect([1,2,3,4].inject {|memo, item| memo - item}).to eq -8
       expect([1,2,3,4].my_rec_inject {|memo, item| memo - item}).to eq -8
+      expect([1,2,3,4].my_itr_inject {|memo, item| memo - item}).to eq -8
     end
 
     it 'can multiply numbers' do
       expect([1,2,3,4].inject {|memo, item| memo * item}).to eq 24
       expect([1,2,3,4].my_rec_inject {|memo, item| memo * item}).to eq 24
+      expect([1,2,3,4].my_itr_inject {|memo, item| memo * item}).to eq 24
     end
   end
 
@@ -23,11 +27,13 @@ describe Array do
     it 'can add numbers' do
       expect([1,2,3,4].inject(:+)).to eq 10
       expect([1,2,3,4].my_rec_inject(:+)).to eq 10
+      expect([1,2,3,4].my_itr_inject(:+)).to eq 10
     end
 
     it 'can minus numbers' do
       expect([1,2,3,4].inject(:-)).to eq -8
       expect([1,2,3,4].my_rec_inject(:-)).to eq -8
+      expect([1,2,3,4].my_itr_inject(:-)).to eq -8
     end
   end
 
@@ -35,16 +41,20 @@ describe Array do
     it 'can add with a starting point' do
       expect([1,2,3,4].inject(10) {|memo, item| memo + item}).to eq 20
       expect([1,2,3,4].my_rec_inject(10) {|memo, item| memo + item}).to eq 20
+      expect([1,2,3,4].my_itr_inject(10) {|memo, item| memo + item}).to eq 20
     end
 
     it 'can minus with a starting point' do
       expect([1,2,3,4].inject(10) {|memo, item| memo - item}).to eq 0
       expect([1,2,3,4].my_rec_inject(10) {|memo, item| memo - item}).to eq 0
+      expect([1,2,3,4].my_itr_inject(10) {|memo, item| memo - item}).to eq 0
+
     end
 
     it 'can multiply with a starting point' do
       expect([1,2,3,4].inject(10) {|memo, item| memo * item}).to eq 240
       expect([1,2,3,4].my_rec_inject(10) {|memo, item| memo * item}).to eq 240
+      expect([1,2,3,4].my_itr_inject(10) {|memo, item| memo * item}).to eq 240
     end
   end
 
@@ -52,11 +62,13 @@ describe Array do
     it 'can add with a starting point and a symbol' do
       expect([1,2,3,4].inject(10, :+)).to eq 20
       expect([1,2,3,4].my_rec_inject(10, :+)).to eq 20
+      expect([1,2,3,4].my_itr_inject(10, :+)).to eq 20
     end
 
     it 'can minus with a starting point and a symbol' do
       expect([1,2,3,4].inject(10, :-)).to eq 0
       expect([1,2,3,4].my_rec_inject(10, :-)).to eq 0
+      expect([1,2,3,4].my_itr_inject(10, :-)).to eq 0
     end
   end
 
