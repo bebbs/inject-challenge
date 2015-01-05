954419d273eb4d6cace06e392f90613640443ba2
Josh Bebbington <josh@focalwebsites.com>
2015-01-05 13:22:59 +0000

    Minor fixes

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -4,7 +4,7 @@ class Array
   def my_rec_inject(*args, &block)
     arr = self.dup
     temp, next_value = arr.shift, 0 if symbol_or_empty?(args)
-    temp, next_value = args[0], 0 if not_symbol_and_empty?(args)
+    temp, next_value = args[0], 0 if not_symbol_or_empty?(args)
     args.map{|arg| block = arg.to_proc if arg.is_a?(Symbol)}
     
     return temp if arr.empty?
@@ -16,7 +16,7 @@ class Array
   def my_itr_inject(*args, &block)
     arr = self.dup
     temp = self[0] if symbol_or_empty?(args)
-    temp = args[0] if not_symbol_and_empty?(args)
+    temp = args[0] if not_symbol_or_empty?(args)
     args.map{|arg| block = arg.to_proc if arg.is_a?(Symbol)}
 
     arr.shift if symbol_or_empty?(args)
@@ -29,6 +29,6 @@ def symbol_or_empty?(args)
   args[0].is_a?(Symbol) || args.empty?
 end
 
-def not_symbol_and_empty?(args)
+def not_symbol_or_empty?(args)
   !args[0].is_a?(Symbol) && !args[0].nil?
 end
