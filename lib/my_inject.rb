38c45729e92ef7e70c9452de3335e613cf2f4a95
Josh Bebbington <josh@focalwebsites.com>
2015-01-05 12:23:50 +0000

    Started refactoring lengthy methods

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -3,8 +3,8 @@ class Array
   # Recursive inject method
   def my_rec_inject(*args, &block)
     arr = self.dup
-    temp, next_value = arr.shift, 0 if args[0].is_a?(Symbol) || args.empty?
-    temp, next_value = args[0], 0 if !args[0].is_a?(Symbol) && !args[0].nil?
+    temp, next_value = arr.shift, 0 if symbol_or_empty?(args)
+    temp, next_value = args[0], 0 if not_symbol_and_empty?(args)
     args.map{|arg| block = arg.to_proc if arg.is_a?(Symbol)}
     
     return temp if arr.empty?
@@ -15,12 +15,20 @@ class Array
   # Iterative inject method
   def my_itr_inject(*args, &block)
     arr = self.dup
-    temp = self[0] if args[0].is_a?(Symbol) || args.empty?
-    temp = args[0] if !args[0].is_a?(Symbol) && !args[0].nil?
+    temp = self[0] if symbol_or_empty?(args)
+    temp = args[0] if not_symbol_and_empty?(args)
     args.map{|arg| block = arg.to_proc if arg.is_a?(Symbol)}
 
-    arr.shift if args[0].is_a?(Symbol) || args.empty?
+    arr.shift if symbol_or_empty?(args)
     arr.each {|x| temp = block.call temp, x}
     temp
   end
+end
+
+def symbol_or_empty?(args)
+  args[0].is_a?(Symbol) || args.empty?
+end
+
+def not_symbol_and_empty?(args)
+  !args[0].is_a?(Symbol) && !args[0].nil?
 end
