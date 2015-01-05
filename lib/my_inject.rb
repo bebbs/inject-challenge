5283a623c1be9b8cc214de4f9ebdf88b4f4daa37
Josh Bebbington <josh@focalwebsites.com>
2015-01-05 14:47:26 +0000

    Post code review refactoring.

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -3,32 +3,30 @@ class Array
   # Recursive inject method
   def my_rec_inject(*args, &block)
     arr = self.dup
-    temp, next_value = arr.shift, 0 if symbol_or_empty?(args)
-    temp, next_value = args[0], 0 if not_symbol_or_empty?(args)
+    memo = symbol_or_empty?(args) ? arr.shift : args[0]
+
     args.map{|arg| block = arg.to_proc if arg.is_a?(Symbol)}
     
-    return temp if arr.empty?
-    block_result = block.call(temp, arr[next_value])
+    return memo if arr.empty?
+    block_result = block.call(memo, arr.first)
     arr.drop(1).my_rec_inject(block_result, args, &block)
   end
 
   # Iterative inject method
   def my_itr_inject(*args, &block)
     arr = self.dup
-    temp = self[0] if symbol_or_empty?(args)
-    temp = args[0] if not_symbol_or_empty?(args)
-    args.map{|arg| block = arg.to_proc if arg.is_a?(Symbol)}
+    memo = symbol_or_empty?(args) ? arr.shift : args[0]
 
-    arr.shift if symbol_or_empty?(args)
-    arr.each {|x| temp = block.call temp, x}
-    temp
+    if symbol = args.find{|arg| arg.is_a?(Symbol)} 
+      block = symbol.to_proc 
+    end
+
+    arr.each {|x| memo = block.call memo, x}
+    memo
   end
-end
 
-def symbol_or_empty?(args)
-  args[0].is_a?(Symbol) || args.empty?
-end
+  def symbol_or_empty?(args)
+    args[0].is_a?(Symbol) || args.empty?
+  end
 
-def not_symbol_or_empty?(args)
-  !args[0].is_a?(Symbol) && !args[0].nil?
 end
